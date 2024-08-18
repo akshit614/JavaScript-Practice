f9fd1349f28acf7c11b6dcd943bc478e3ba31340
Aksh4code <akkisingh8266@gmail.com>
2024-08-18 11:13:38 +0000

    let_const_var

---
+++ basics_js/variable.js
@@ -0,0 +1,14 @@
+const account_id = 456123
+let name = "Akshit"
+let address
+
+name = "huhuhuhu"
+// account_id = 562389  not allowed in js
+
+/*
+Const for a constant that does not change
+Let for all variable that can be change further
+dont use var because it has scope problems
+*/
+
+console.table([name, account_id, address])
