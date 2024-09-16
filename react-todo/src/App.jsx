6c77104f6782712595d18eabe4e7efe1df683338
Aksh4code <akkisingh8266@gmail.com>
2024-09-16 23:31:34 +0530

    added some functionalities

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [totos, setTodos] = useState([{
+    id: 1,
+    title: "Todo 1",
+    description : "Todo 1 description",
+    completed: false
+  },{
+    id: 2,
+    title: "Todo 2",
+    description : "Todo 2 description",
+    completed: true
+  },{
+    id: 3,
+    title: "Todo 3",
+    description : "Todo 3 description",
+    completed: false
+  }])
+
+  function addTodos() {
+    setTodos([...totos, {
+      id: 4,
+      title: "Todo 4",
+      description : "Todo 4 description",
+      completed: true
+    }])   
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={addTodos} >Add Todo</button>
+        {totos.map(function(todo) { 
+          return <Todo key={todo.id} title={todo.title} description={todo.description} />
+        })}
+        {/* { <Todo title={totos[0].title} description={totos[0].description}></Todo>
+        <Todo title={totos[1].title} description={totos[1].description}></Todo>
+        <Todo title={totos[2].title} description={totos[2].description}></Todo> */} 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+  )
+}
+
+/**
+ * Component to render a todo item
+ * @param {{title: string, description: string}} props
+ * @returns {JSX.Element}
+ */
+function Todo(props){
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+    </div>
   )
 }
 
