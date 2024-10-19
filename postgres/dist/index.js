08e6c34f5e23574eab2e13a6f5cd293df699458e
Aksh4code <akkisingh8266@gmail.com>
2024-10-19 10:24:14 +0530

    updated queries

--- postgres/dist/index.js
+++ postgres/dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+// Using postGres DB in a node project
+// using neonDB database and pg library
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,21 +15,26 @@ const pg_1 = require("pg");
 const client = new pg_1.Client({
     connectionString: "postgresql://TryPostgresDB_owner:HTFeGktv04Si@ep-falling-rain-a47xbcpw.us-east-1.aws.neon.tech/TryPostgresDB?sslmode=require"
 });
+// function to create table in database using sql query
 function createUserTable() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.connect();
-        const res = yield client.query(`
-            CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(255) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-            )
-        `);
+        try {
+            yield client.query(`CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(255) UNIQUE NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+                )`);
+        }
+        catch (error) {
+            console.error(error.message);
+        }
     });
 }
 // createUserTable();
+// function to insert values to table in database using sql query
 function insertIntoTable() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.connect();
